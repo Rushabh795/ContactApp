705e6746bc1d7810d0823f5b2617b66acc5d49c7
Rushabh Shah <rushabh795@gmail.com>
2022-07-23 13:38:45 -0400

    -Added new Images -Create Add new Contact Layout -Create delete menu

--- app/src/main/java/com/rushabh/contactapp/AddContactActivity.java
+++ app/src/main/java/com/rushabh/contactapp/AddContactActivity.java
@@ -1,8 +1,17 @@
 package com.rushabh.contactapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AddContactActivity extends AppCompatActivity {
 
@@ -10,5 +19,40 @@ public class AddContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main , menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+switch (item.getItemId())
+{
+    case R.id.action_delete:
+        MaterialAlertDialogBuilder materialAlertDialogBuilder = new MaterialAlertDialogBuilder(AddContactActivity.this);
+        materialAlertDialogBuilder.setTitle("Are you sure want to delete ?");
+        materialAlertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+Snackbar.make(AddContactActivity.this.findViewById(android.R.id.content) , "Done",Snackbar.LENGTH_LONG).show();
+            }
+        });
+        materialAlertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+//        materialAlertDialogBuilder.setBackground(getResources().get)
+        materialAlertDialogBuilder.show();
+}
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/rushabh/contactapp/MainActivity.java
+++ app/src/main/java/com/rushabh/contactapp/MainActivity.java
@@ -10,20 +10,18 @@ import com.github.clans.fab.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fabAddNew;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bindID();
     }
-
     private void bindID() {
         fabAddNew = findViewById(R.id.fabAddNew);
         fabAddNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ContactDetailsActivity.class);
+                Intent intent = new Intent(MainActivity.this,AddContactActivity.class);
                 startActivity(intent);
             }
         });
